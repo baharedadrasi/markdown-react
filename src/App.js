1fdf204c2a11f0bf5f49a3d8e7372a1d599183f5
bahare <baharedadrasi@gmail.com>
2022-06-24 13:02:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [markdown, setMarkdown] = React.useState('# markdown preview');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => {
+            setMarkdown(e.target.value);
+          }}
+        ></textarea>
+        <article className="reslt">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
   );
 }
 
